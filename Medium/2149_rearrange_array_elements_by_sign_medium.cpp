f17ec87ab985b3ca511641ba883196db618e98c1
Swasti Pandey <swastipandey69@gmail.com>
2026-04-26 13:14:53 +0530

    Added 2149 rearrange array elements by sign (medium)

---
+++ Medium/2149_rearrange_array_elements_by_sign_medium.cpp
@@ -0,0 +1,27 @@
+// 2149. Rearrange Array Elements by Sign
+// Difficulty: Medium
+// Approach: Place positives at even indices, negatives at odd indices
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+
+        int posIndex = 0, negIndex = 1;
+
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < 0) {
+                ans[negIndex] = nums[i];
+                negIndex += 2;
+            } else {
+                ans[posIndex] = nums[i];
+                posIndex += 2;
+            }
+        }
+
+        return ans;
+    }
+};
