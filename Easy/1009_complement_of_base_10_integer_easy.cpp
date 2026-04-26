5e9bd7b39f8ec75b765fc4fc14e196ce83e33e14
Swasti Pandey <swastipandey69@gmail.com>
2026-04-26 12:41:50 +0530

    Added 1009 complement of base 10 integer (easy)

---
+++ Easy/1009_complement_of_base_10_integer_easy.cpp
@@ -0,0 +1,18 @@
+// 1009. Complement of Base 10 Integer
+// Difficulty: Easy
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (n == 0) return 1;
+
+        int mask = 0, temp = n;
+
+        while (temp > 0) {
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+
+        return (~n) & mask;
+    }
+};
